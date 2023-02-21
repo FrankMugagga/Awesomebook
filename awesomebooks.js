6c4318ee94fad6ed1e250a4ae5cbcaba35880108
FrankMugagga <frank.mugagga@gmail.com>
2023-02-21 10:07:44 +0300

    worked on the adding of storage

--- awesomebooks.js
+++ awesomebooks.js
@@ -1,36 +1,50 @@
 const add_button = document.getElementById('add');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+
+const title = document.getElementById('title').value.trim();
+const author = document.getElementById('author').value.trim();
+
+const titlError = document.getElementById('fill_title');
+const authorError = document.getElementById('fill_author');
+const error = document.getElementById('error')
+
 const list = document.querySelector('.book_display');
 
 var book_array = [];
 
 add_button.addEventListener('click', (e)=> {
-    e.preventDefault()
-    
-    const book = {
-        book_title: title.value,
-        book_author: author.value,
-    };
-    
-    book_array.push(book);
-    localStorage.setItem('books', JSON.stringify(book_array));    
-     
+    e.preventDefault();
+
+    if(title === '' && author === ''){
+        
+        error.innerHTML = 'Please enter missing value';
+    }
+    else {
+        alert('we got it');
+        error.innerHTML = '';  
+        const book = {
+            book_title: title,
+            book_author: author
+        };
+        
+        book_array.push(book);
+        localStorage.setItem('books', JSON.stringify(book_array));    
+    }     
 });
+
+
 var list_display = '';
 window.addEventListener('load', (event) => {
 
     var stored = JSON.parse(localStorage.getItem('books'));
-    console.log(stored);
-   // console.log('The page has fully loaded');
-   stored.forEach((book) =>{
-     list_display = `
-     <li>${stored.title}<br>${stored.author}</li>
-     `;
-   });
-   list.appendChild(list_display);
+   stored.forEach((book, index) => {
+    const li = `<li>${stored.title + stored.author}</li>`;
+    
+    list.innerHTML = li;
+   }
+   );
+   
+})
 
-});
 
 
 
